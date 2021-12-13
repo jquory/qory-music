97b4f1fdcbb1ce55bff88fc82d1a7e49e8256b86
jquory <qorey@192.168.1.4>
2021-12-13 18:36:13 +0700

    bagusin icon dan judul

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>jQuory Music</title>
+        <link rel="icon" href="/qicon.ico" />
       </Head>
 
       <main className="flex">
